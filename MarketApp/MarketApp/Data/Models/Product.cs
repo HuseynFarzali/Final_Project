0699edb53150b38e184b69576758144b9b866604
HuseynFarzali <huseynyf@code.edu.az>
2023-07-24 08:11:06 -0700

    Create the project layout

---
+++ MarketApp/MarketApp/Data/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MarketApp.Data.Enums;
+
+namespace MarketApp.Data.Models
+{
+    public class Product
+    {
+        public string ProductName { get; set; }
+        public decimal ProductValue { get; set; }
+        public Category ProductCategory { get; set; }
+        public int ProductCount { get; set; }
+        public int ID { get; set; }
+    }
+}
---
+++ MarketApp/MarketApp/Data/Models/Sale.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketApp.Data.Models
+{
+    public class Sale
+    {
+        public int ID { get; set; }
+        public int FinalPrice { get; set; }
+
+        public List<Product> SaleItems = new List<Product>();
+        public DateTime Date { get; set; }
+    }
+}
